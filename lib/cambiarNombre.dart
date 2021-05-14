9c6ee107077b55e29b56649677bdeb36cd5ba8df
Pol Gomez <43774035+lakoGM@users.noreply.github.com>
2021-05-14 10:24:42 +0200

    pantallazo cambiar nombre correjido

--- lib/cambiarNombre.dart
+++ lib/cambiarNombre.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/requests.dart';
+import 'dart:async';
 
 class cambiarNombre extends StatefulWidget {
   cambiarNombre();
@@ -11,26 +12,24 @@ class _cambiarNombreState extends State<cambiarNombre> {
   String username;
   final newUsername = TextEditingController();
   final _formKeyUsername = GlobalKey<FormState>();
+  StreamController _postController;
 
   @override
   void initState() {
-    //globals.token = widget.token;
+    _postController = new StreamController();
     getInfo();
-    //num_gradiente = _publicacion['gradient'][0];
+    super.initState();
   }
 
   void getInfo() async {
-    //await Future.delayed(Duration(seconds: 1));
-    Text("Welcome");
     getUsername().then((result) {
       setState(() => username = result);
+      _postController.add(result);
     });
   }
 
   @override
   void dispose() {
-    // Clean up the controller when the widget is removed from the
-    // widget tree.
     newUsername.dispose();
     super.dispose();
   }
@@ -54,83 +53,110 @@ class _cambiarNombreState extends State<cambiarNombre> {
           style: TextStyle(fontSize: 16.0, fontFamily: 'Glacial Indifference'),
         ),
       ),
-      body: SingleChildScrollView(
-        child: Form(
-          key: _formKeyUsername,
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 15.0, right: 15.0, top: 20, bottom: 13),
-              ),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Cambio de nombre de usuario",
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 22.0,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ]),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 15.0, right: 15.0, top: 20, bottom: 13),
-              ),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: Text('Nombre de usuario actual: ' + username),
-              ),
-              new Padding(padding: EdgeInsets.only(top: 10.0)),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Nuevo nombre',
-                  hintText: 'Introduce tu nuevo nombre de usuario',
-                  border: OutlineInputBorder(
-                    borderRadius: new BorderRadius.circular(5.0),
-                    borderSide: new BorderSide(),
-                  ),
-                ),
-                controller: newUsername,
-                validator: validateUsername,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 15.0, right: 15.0, top: 20, bottom: 13),
-              ),
-              Container(
-                height: 50,
-                width: 250,
-                decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(20)),
-                child: FlatButton(
-                  onPressed: () async {
-                    // Validate returns true if the form is valid, otherwise false.
-                    if (_formKeyUsername.currentState.validate()) {
-                      _formKeyUsername.currentState.save();
-                      Scaffold.of(_formKeyUsername.currentContext).showSnackBar(
-                          SnackBar(content: Text('Procesando cambios')));
-                      print(newUsername.text);
-                      editUsername(newUsername.text);
+      body: StreamBuilder(
+        stream: _postController.stream,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          print('Has error: ${snapshot.hasError}');
+          print('Has data: ${snapshot.hasData}');
+          print('Snapshot Data ${snapshot.data}');
 
-                      Navigator.of(context).pop(true);
-                    }
-                  },
-                  child: Text(
-                    'Confirmar',
-                    style: TextStyle(color: Colors.white, fontSize: 22),
-                  ),
+          if (snapshot.hasError) {
+            return Text(snapshot.error);
+          }
+
+          if (snapshot.hasData) {
+            return SingleChildScrollView(
+              child: Form(
+                key: _formKeyUsername,
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 15.0, right: 15.0, top: 20, bottom: 13),
+                    ),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Cambio de nombre de usuario",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ]),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 15.0, right: 15.0, top: 20, bottom: 13),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(10),
+                      child: Text('Nombre de usuario actual: ' + username),
+                    ),
+                    new Padding(padding: EdgeInsets.only(top: 10.0)),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Nuevo nombre',
+                        hintText: 'Introduce tu nuevo nombre de usuario',
+                        border: OutlineInputBorder(
+                          borderRadius: new BorderRadius.circular(5.0),
+                          borderSide: new BorderSide(),
+                        ),
+                      ),
+                      controller: newUsername,
+                      validator: validateUsername,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 15.0, right: 15.0, top: 20, bottom: 13),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 250,
+                      decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(20)),
+                      child: FlatButton(
+                        onPressed: () async {
+                          // Validate returns true if the form is valid, otherwise false.
+                          if (_formKeyUsername.currentState.validate()) {
+                            _formKeyUsername.currentState.save();
+                            Scaffold.of(_formKeyUsername.currentContext)
+                                .showSnackBar(SnackBar(
+                                    content: Text('Procesando cambios')));
+                            print(newUsername.text);
+                            editUsername(newUsername.text);
+
+                            Navigator.of(context).pop(true);
+                          }
+                        },
+                        child: Text(
+                          'Confirmar',
+                          style: TextStyle(color: Colors.white, fontSize: 22),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 110,
+                    ),
+                  ],
                 ),
               ),
-              SizedBox(
-                height: 110,
-              ),
-            ],
-          ),
-        ),
+            );
+          }
+
+          if (!snapshot.hasData) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ],
+            );
+          }
+        },
       ),
     );
   }
