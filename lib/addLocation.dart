15487cc04724da30000832174b7de1cbb59c40f6
1490917 <72441147+1490917@users.noreply.github.com>
2021-05-28 12:44:36 +0200

    Arreglado overflow

--- lib/addLocation.dart
+++ lib/addLocation.dart
@@ -23,62 +23,42 @@ class AddLocation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            'ECOPROTECT',
-            style:
-                TextStyle(fontSize: 16.0, fontFamily: 'Glacial Indifference'),
-          ),
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'ECOPROTECT',
+          style: TextStyle(fontSize: 16.0, fontFamily: 'Glacial Indifference'),
         ),
-        body: Column(children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Text("Añade la ubicación:"),
+      ),
+      body: Column(children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Text("Añade la ubicación:"),
+        ),
+        Center(
+          child: SizedBox(
+            height: 383.0,
+            child: SearchMapPlaceWidget(
+              language: 'es',
+              iconColor: Colors.green,
+              placeholder: "Introduce una ubicación",
+              apiKey: "AIzaSyCkG1TBTljazmME6wVvjTTw_yBuYp5b6Qg",
+              onSelected: (Place place) async {
+                Geolocation geolocation = await place.geolocation;
+                final geocoding = GoogleMapsGeocoding(
+                    apiKey: 'AIzaSyCkG1TBTljazmME6wVvjTTw_yBuYp5b6Qg');
+                final address = await geocoding.searchByPlaceId(place.placeId);
+                final ubiName = address.results[0].formattedAddress;
+                globals.ubication = ubiName;
+                globals.latitude = address.results[0].geometry.location.lat;
+                globals.longitude = address.results[0].geometry.location.lng;
+                Navigator.of(context).pop(ubication.text);
+              },
+            ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: 
-            SearchMapPlaceWidget(
-                    language: 'es',
-                    iconColor: Colors.green,
-                    placeholder: "Introduce una ubicación",
-                    apiKey: "AIzaSyCkG1TBTljazmME6wVvjTTw_yBuYp5b6Qg",
-                    onSelected: (Place place) async {
-                      Geolocation geolocation = await place.geolocation;
-                      final geocoding = GoogleMapsGeocoding(apiKey: 'AIzaSyCkG1TBTljazmME6wVvjTTw_yBuYp5b6Qg');
-                      final address = await geocoding.searchByPlaceId(place.placeId);
-                      final ubiName =  address.results[0].formattedAddress;
-                      globals.ubication = ubiName;
-                      globals.latitude = address.results[0].geometry.location.lat;
-                      globals.longitude = address.results[0].geometry.location.lng;
-                      Navigator.of(context).pop(ubication.text);
-                    },
-                    
-                  ),
-            
-            /*TextFormField(
-              maxLength: 30,
-              controller: ubication,
-              decoration: InputDecoration(
-                hintText: 'Ubicación',
-              ),*/
-            )
-        ]),
-        /*
-        floatingActionButton: Transform.scale(
-          scale: 1.2,
-          alignment: Alignment.bottomCenter,
-          child: FloatingActionButton(
-            onPressed: () {
-              globals.ubication = geolocation;        
-              Navigator.of(context).pop(ubication.text);
-            },
-
-            child: Icon(Icons.check_outlined),
-            backgroundColor: Color.fromRGBO(100, 211, 83, 1),
-          ),
-        )*/);
+        )
+      ]),
+    );
   }
 }
